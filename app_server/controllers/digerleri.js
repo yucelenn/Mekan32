07f6ccdc504de79e6cd0401e08bbd9496782e076
yucelenn <yucelenn@hotmail.com>
2020-12-01 01:09:22 +0300

    2. gonderim

---
+++ app_server/controllers/digerleri.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+const hakkinda=function(req, res, next) {
+  res.render('hakkinda', { title: 'Hakkinda' });
+}
+
+module.exports={
+hakkinda
+}
